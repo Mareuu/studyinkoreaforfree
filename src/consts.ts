2d53ddd2cd41df3440f4bbee92a4c8ba71969c28
Mareuu <marhamidhatiey@gmail.com>
2026-07-24 15:02:59 +0000

    Jul 25, 2026, 12:02 AM

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Study in Korea for Free';
+export const SITE_DESCRIPTION = 'Your complete guide to direct 100% tuition waivers, professor lab stipends, and research assistantships.';
